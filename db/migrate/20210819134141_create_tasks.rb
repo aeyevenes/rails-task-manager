7d9cf09fb2669c8bd5639b0c93f7127830f74c4f
alejandro yevenes <aeyevenes@gmail.com>
2021-08-19 11:22:39 -0400

    change false bu boolean to Tasks

--- db/migrate/20210819134141_create_tasks.rb
+++ db/migrate/20210819134141_create_tasks.rb
@@ -3,7 +3,7 @@ class CreateTasks < ActiveRecord::Migration[6.1]
     create_table :tasks do |t|
       t.string :tittle
       t.string :details
-      t.false :completed
+      t.boolean :completed
 
       t.timestamps
     end
